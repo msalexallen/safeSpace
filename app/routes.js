36634cc9c2d20c4c7d523988a7f5e24eab2421e4
Alex <msalexallen@gmail.com>
2015-03-05 15:55:10 -0800

    Added database handling and most of image serving

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,17 @@ var sys = require('sys');
 var sqlite3 = require('sqlite3').verbose();
 var mkdirp = require('mkdirp');
 
+var file = "userDatabase.db";
+var fs = require("fs");;
+var db = new sqlite3.Database(file);
+
 module.exports = function(app, passport) {
 
 	//Default page is always index
 	app.get('/', function(req, res) {
 		res.render('index.ejs');
+		req.session.username = null;
+		req.session.password = null;
 	});
 
 	//Get user allows us to authenticate the user before 
@@ -18,21 +24,72 @@ module.exports = function(app, passport) {
 	});
 
 	app.post('/login/username', function(req,res) {
-		req.user = req.body.username;
+		user = req.body.username;
+		//add in an if username is a string of only numbers and letters
+		if (/^\w+$/.test(user)){
+			db.all("SELECT * FROM local WHERE username = '"+user+"'", function(err, rows) {
+	            if (err){
+	                console.log(err)
+	                res.redirect('/');
+	            }
+	            if (rows == 0){
+	                console.log("Exists");
+	                res.render('getun.ejs', { message: 
+	                	req.flash('loginMessage', 'That username does not exist.') });
+	            } else {
+					req.session.username = user;
+					req.session.password = rows[0].password;
+					res.redirect('/login/passimg');
+				}
+			});
+		}
+		else {
+			res.render('getun.ejs', { message: 'Dost thou not knoweth thine own username?' });
+		}
+	});
 
-		res.redirect('/login');
+	//Serve the user image from the session (and make a fuss if a session username isn't set)
+	app.get('/login/image.png', function(req, res) {
+		user = req.session.username;
+		if (user != null){
+			//res.sendfile('./userFiles/'+user+'/passImg.png');
+			res.end('./userFiles/'+user+'/passImg.png', 'binary');
+		}
+		else{
+			res.render('getun.ejs', { message: 'Please enter your username before your password.' });
+		}
 	});
 
 	//Login handles serving the image and validating the entered points
-	app.get('/login', function(req, res) {
+	app.get('/login/passimg', function(req, res) {
 		res.render('login.ejs', { message: req.flash('loginMessage') }); 
 	});
 
-	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/dashboard', 
-		failureRedirect : '/login', 
-		failureFlash : true
-	}));
+	app.post('/login/passimg', function(req,res) {
+		user = req.session.username;
+		pass = req.session.password;
+		passPoints = req.body.content;
+		if ((user != null) && (pass != null)){
+			pass = JSON.parse(pass);
+			passPoints = JSON.parse(passPoints);
+
+			if (pass.length == passPoints.length){
+				for (i = 0; i < pass.length; i++) {
+					if (!(pass[i].x <= passPoints[i].x <= (pass[i].w + pass[i].x)) ||
+						!(pass[i].y <= passPoints[i].y <= (pass[i].h + pass[i].y))){
+						res.render('login.ejs', { message: 'Invalid password.' });
+					} 
+				}
+				res.redirect('/login/success');
+			}
+			else{
+				res.render('login.ejs', { message: 'Invalid password.' });
+			}
+		}
+		else{
+			res.render('getun.ejs', { message: 'Please enter your username before your password.' });
+		}
+	});
 
 	//Username handles checking username for valid text and uniqueness
 	app.get('/signup/username', function(req, res) {
@@ -41,9 +98,25 @@ module.exports = function(app, passport) {
 
 	app.post('/signup/username', function(req,res) {
 		user = req.body.username;
-		req.session.username = user;
-
-		res.redirect('/signup/passimg');
+		//add in an if username is a string of only numbers and letters
+		if (/^\w+$/.test(user)){
+			db.all("SELECT * FROM local WHERE username = '"+user+"'", function(err, rows) {
+	            if (err){
+	                console.log(err)
+	                res.redirect('/');
+	            }
+	            if (rows !=0){
+	                console.log("Exists");
+	                res.render('createun.ejs', { message: 
+	                	req.flash('signupMessage', 'That username is already in use.') });
+	            } else {
+					req.session.username = user;
+					res.redirect('/signup/passimg');
+				}
+			});
+		} else {
+			res.render('createun.ejs', { message: 'Invalid character. Letters and numbers only, please.' });
+		}
 	});
 
 	//Sign up handles the password image saving, the database entries, and
@@ -58,24 +131,42 @@ module.exports = function(app, passport) {
 		console.log(user);
 		passShapes = req.body.content;
 		var img = req.body.image;
+		//Verify that they haven't bypassed the username page
+		if (user != null){
+			//Verify that the password has at least 3 objects
+			if (JSON.parse(passShapes).length > 2){
+				//get a base64 buffer of the password image
+				var data = img.replace(/^data:image\/\w+;base64,/, "");
+				var buf = new Buffer(data, 'base64');
 
-		var data = img.replace(/^data:image\/\w+;base64,/, "");
-		var buf = new Buffer(data, 'base64');
+				//Create the file and directory to store the image files
+				//on the server and fill the file from the buffer
+				mkdirp("./userFiles/"+user+"/", function (err) {
+		            if (err) 
+		                console.log(err)
+		        });
+				fs.open('./userFiles/'+user+'/passImg.png', "w");
+				fs.writeFileSync('./userFiles/'+user+'/passImg.png', buf);
+		        
+		        //add the username and stringified password shapes to the database
+		        db.all("INSERT INTO local (username, password) VALUES " +
+		            "('"+user+"', '"+passShapes+"')", function(err,rows){});
 
-		mkdirp("./views/userFiles/"+user+"/", function (err) {
-            if (err) 
-                console.log(err)
-        });
-		fs.open('./views/userFiles/'+user+'/passImg.png', "w");
-		
-		//write the info
-		fs.writeFileSync('./views/userFiles/'+user+'/passImg.png', buf);
-
-		req.session.username = null;
-		res.redirect('/signup/success');
+		        //remove the username from the session and send to a success page
+				req.session.username = null;
+				res.redirect('/signup/success');
+			}
+			else {
+				//Invalid password? Redirect to password page again.
+				res.render('signup.ejs', { message: 'Passwords must be three or more regions.' });
+			}
+		}
+		else{
+			es.render('createun.ejs', { message: 'Set a username before creating a password.' });
+		}
 	});
 
-	//A success page 
+	//A success page for sign up
 	app.get('/signup/success', function(req, res) {
 		res.render('signupsuccess.ejs');
 	});
@@ -84,12 +175,13 @@ module.exports = function(app, passport) {
 		res.redirect('/');
 	});
 
-}
+	//A success page for login
+	app.get('/login/success', function(req, res) {
+		res.render('signupsuccess.ejs');
+	});
 
-function isLoggedIn(req, res, next) {
+	app.post('/login/success', function(req, res) {
+		res.redirect('/');
+	});
 
-	if (req.isAuthenticated())
-		return next();
-
-	res.redirect('/');
 }
---
+++ public/javascript/imageserve.js
@@ -0,0 +1,91 @@
+window.addEventListener('load', function() {
+	// Get the context of the editable canvas in the html
+	var canvas = document.querySelector('#editableCanvas');
+	var context = canvas.getContext('2d');
+	
+	//Give it the same features as its enclosing div
+	var drawing1 = document.querySelector('#canvasDiv');
+	var drawingStyle = getComputedStyle(drawing1);
+
+	canvas.width = parseInt(drawingStyle.getPropertyValue('width'));
+	canvas.height = parseInt(drawingStyle.getPropertyValue('height'));
+	
+	var squares = [];
+	var op = 0;
+
+	// Make a temporary canvas (same feats as canvas) to store the changes before they 
+	// get added to the final document ex sample lines with the line tool before you release the mouse
+	var tempCanvas = document.createElement('canvas');
+	var tempContext = tempCanvas.getContext('2d');
+	tempCanvas.id = 'tempCanvas';
+	tempCanvas.width = canvas.width;
+	tempCanvas.height = canvas.height;
+
+	//add in the temp image
+	var o1 =0;
+	var offset = 0;
+	var img = myImg;
+
+	img.change = function () {
+		console.log(img.height);
+	    context.drawImage(img,0,0);
+    	canvas.height = img.height;
+    	tempCanvas.height = img.height;
+    	o1 = img.width - canvas.width;
+    	render();
+	}
+
+	function render(){
+	    context.clearRect(0,0,canvas.width, canvas.height);
+	    tempContext.clearRect(0, 0, tempContext.canvas.width, tempContext.canvas.height);
+	    op = Math.max(offset-o1, 0);
+	    context.drawImage(img, offset, 0, canvas.width-op, img.height, 0, 0, canvas.width-op, img.height)
+	    context.drawImage(img,0,0,op, img.height,canvas.width-op ,0,op,img.height);
+
+		passPoints.value = JSON.stringify(squares);
+	}
+		//tempContext.stroke();
+	
+	drawing1.appendChild(tempCanvas);
+
+	// Dictionary to define a mouse location (x,y)
+	var mouse = {x: 0, y: 0};
+	
+	// Set the code to capture the initial mouse movement
+	tempCanvas.addEventListener('mousemove', function(e) {
+		mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+		mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+	}, false);
+	
+	// Add a listener to the temporary canvas for when the mouse is down
+	tempCanvas.addEventListener('mouseclick', function(e) {
+
+		// Get the x and y of the mouse where it is down
+		mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+		mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+		
+		// Add the x and y to the crayon 'visited' points
+		x1Point = mouse.x + offset;
+		if (x1Point > img.width){
+			x1Point = x1Point - img.width;
+		}
+		squares.push({x: x1Point, y: mouse.y});
+
+	}, false);
+
+	window.onkeydown = function(e) {
+	    var key = e.keyCode;
+	    if (key == 37) {
+	        offset -= 100;
+	    } else if (key == 39) {
+	        offset += 100;
+	    }
+	    if(offset >= img.width){
+	        offset= offset%img.width;
+	    } else if (offset <= 0) {
+	        offset=img.width+offset;
+	    }
+	    render();
+	}	
+
+}, false);
